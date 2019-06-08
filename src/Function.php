37ced3552dc55dd5de587e14025c9ff8c0d17fd0
DimaSSH <30565695+OppaiCyber@users.noreply.github.com>
2019-06-08 04:25:08 -0700

    Update Function
    add Function PumpDump

--- src/Function.php
+++ src/Function.php
@@ -23,10 +23,7 @@ function PumpDump($last,$day){
 function IndodaxPrices(){
 	$decode = json_decode(seeURL("https://indodax.com/api/summaries"), true);
 	$array = array('btc_idr','ten_idr','abyss_idr','act_idr','ada_idr','aoa_idr','bcd_idr','bchabc_idr','bchsv_idr','bnb_idr','btg_idr','bts_idr','btt_idr','cro_idr','drk_idr','dax_idr','doge_idr','eth_idr','eos_idr','etc_idr','gsc_idr','gxs_idr','hpb_idr','ignis_idr','inx_idr','ltc_idr','neo_idr','npxs_idr','nxt_idr','ont_idr','pxg_idr','qtum_idr','rvn_idr','scc_idr','stq_idr','sumo_idr','trx_idr','usdc_idr','usdt_idr','vex_idr','waves_idr','str_idr','nem_idr','xdce_idr','xrp_idr','xzc_idr','bts_btc','drk_btc','doge_btc','eth_btc','ltc_btc','nxt_btc','sumo_btc','ten_btc','nem_btc','str_btc','xrp_btc');
-
 	$arrayChange = array('btcidr','tenidr','abyssidr','actidr','adaidr','aoaidr','bcdidr','bchabcidr','bchsvidr','bnbidr','btgidr','btsidr','bttidr','croidr','drkidr','daxidr','dogeidr','ethidr','eosidr','etcidr','gscidr','gxsidr','hpbidr','ignisidr','inxidr','ltcidr','neoidr','npxsidr','nxtidr','ontidr','pxgidr','qtumidr','rvnidr','sccidr','stqidr','sumoidr','trxidr','usdcidr','usdtidr','vexidr','wavesidr','stridr','nemidr','xdceidr','xrpidr','xzcidr','btsbtc','drkbtc','dogebtc','ethbtc','ltcbtc','nxtbtc','sumobtc','tenbtc','nembtc','strbtc','xrpbtc');
-
-
 
 	$main = $decode['tickers'];
 	$text = 'Prices List<br>';
@@ -40,8 +37,6 @@ function IndodaxPrices(){
 
 	}
 
-// print_r($text);
 } // End Func
-// IndodaxPrices();
 
 ?>
