1d30858c16902900ed53b9af94ea840e2ce88c79
DimaSSH <30565695+OppaiCyber@users.noreply.github.com>
2019-06-08 03:02:14 -0700

    Create Function.php

---
+++ src/Function.php
@@ -0,0 +1,11 @@
+<?php
+
+function seeURL($url){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    return curl_exec($ch);
+}
+
+?>
