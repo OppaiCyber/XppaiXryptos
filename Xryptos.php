152dd3d0ddf3d48137342d442f64c54b0f453f42
DimaSSH <30565695+OppaiCyber@users.noreply.github.com>
2019-06-08 05:33:59 -0700

    Update Xryptos.php
    - add /gas Command

--- Xryptos.php
+++ Xryptos.php
@@ -1,9 +1,17 @@
 <?php
 require_once __DIR__.'/src/PHPTelebot.php';
-require_once 'func.php';
-$bot = new PHPTelebot('ur telegram bot token', 'ur bot username'); 
+require_once __DIR__.'/src/Function.php';
+$bot = new PHPTelebot('ur bot token', 'ur bot username');
 
 // Command without Function
+$bot->cmd('hontoni','yokatta');
+
+// Command with Function
+
+$bot->cmd('/gas', function(){
+        $options = ['parse_mode' => 'html','reply' => true,'disable_web_page_preview' => true];
+            return Bot::sendMessage(gasChecker(),$options);
+});
 
 
 $bot->run();
