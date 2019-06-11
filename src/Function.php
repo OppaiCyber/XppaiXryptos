abbaf190a4ed93213299406955abc0ac7325a726
DimaSSH <30565695+OppaiCyber@users.noreply.github.com>
2019-06-10 21:50:23 -0700

    Remove Unnecessary Function

--- src/Function.php
+++ src/Function.php
@@ -8,22 +8,13 @@ function seeURL($url){
     return curl_exec($ch);
 }
 
-function IDRFormula($num){
-	return number_format($num,2,',','.');
-}
-
 function PumpDump($percent){
-	if ($percent > 0) {return "🍀";}else{return "🥀";}
-}
-
-function percentChange($last,$day){
-	$increase = $last - $day;
-	$percent = $increase / $day * 100;
-	return round($percent,2)."%";
+    if ($percent > 0) {return "🍀";}else{return "🥀";}
 }
 
 function gasChecker(){
     $getData = json_decode(seeURL("https://ethgasstation.info/json/ethgasAPI.json"),true);
+
     $safeLow = $getData['safeLow'] / 10;
     $fastest = $getData['fastest'] / 10;
     $medium = $getData['average'] / 10;
@@ -32,42 +23,37 @@ function gasChecker(){
     return $result;
 }
 
+
 function priceChecker($coin){
     $coinx = strtoupper($coin);
     $getData = seeURL("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=$coinx&tsyms=BTC,USD,IDR");
     $decode = json_decode($getData,TRUE);
     @$error = $decode['HasWarning'];
-
     if ($error == 1) {
-    	return "❌ This coin is not listed on exchanges we support ❌";
+        return "❌ This coin is not listed on exchanges we support ❌";
     }else{
-	    $usd = $decode["RAW"][$coinx]["USD"];
-	    $priceUSD = $usd['PRICE'];
-	    $changesUSD = $usd['CHANGEPCTDAY'];
-	    $lowDayUSD = $usd['LOWDAY'];
-	    $highDayUSD = $usd['HIGHDAY'];
-
-	    $idr = $decode["RAW"][$coinx]["IDR"];
-	    $priceIDR = $idr['PRICE'];
-	    $changesIDR = $idr['CHANGEPCTDAY'];
-	    $lowDayIDR = $idr['LOWDAY'];
-	    $highDayIDR = $idr['HIGHDAY'];
-
-	    if ($priceIDR > 1000000) {
-	        $priceIDR = $priceIDR / 1000000;
-	        $lowDayIDR = $lowDayIDR / 1000000;
-	        $highDayIDR = $highDayIDR / 1000000;
-	        $tail = "jt IDR";
-	    }else{
-	        $tail = "IDR";
-	    }
-
-	$result  = "<code>Stats $coinx : \n";
+        $usd = $decode["RAW"][$coinx]["USD"];
+        $priceUSD = $usd['PRICE'];
+        $changesUSD = $usd['CHANGEPCTDAY'];
+        $lowDayUSD = $usd['LOWDAY'];
+        $highDayUSD = $usd['HIGHDAY'];
+        $idr = $decode["RAW"][$coinx]["IDR"];
+        $priceIDR = $idr['PRICE'];
+        $changesIDR = $idr['CHANGEPCTDAY'];
+        $lowDayIDR = $idr['LOWDAY'];
+        $highDayIDR = $idr['HIGHDAY'];
+        if ($priceIDR > 1000000) {
+            $priceIDR = $priceIDR / 1000000;
+            $lowDayIDR = $lowDayIDR / 1000000;
+            $highDayIDR = $highDayIDR / 1000000;
+            $tail = "jt IDR";
+        }else{
+            $tail = "IDR";
+        }
+    $result  = "<code>Stats $coinx : \n";
     $result .= PumpDump($changesUSD).number_format($changesUSD,2)."% | $ $priceUSD \n".PumpDump($changesIDR).number_format($changesIDR,2)."% | ".number_format($priceIDR,2,',','.')." $tail \nHigh : ".number_format($highDayIDR,2,',','.')." $tail | $ $highDayUSD \nLow : ".number_format($lowDayIDR,2,',','.')." $tail | $ $lowDayUSD</code>";
     return $result;
     }// end of else
-
     
 } // end func
 
-?>
