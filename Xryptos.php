3dc1219e76ea7c4a732cf9d16c5335f89efdc3e6
DimaSSH <30565695+OppaiCyber@users.noreply.github.com>
2019-06-24 17:55:41 -0700

    add getNews command

--- Xryptos.php
+++ Xryptos.php
@@ -13,6 +13,11 @@ $bot->cmd('/gas', function(){
                 return Bot::sendMessage(gasChecker(),$options);
 });
 
+$bot->cmd('/news', function(){
+        $options = ['parse_mode' => 'html','reply' => true,'disable_web_page_preview' => true];
+                return Bot::sendMessage(latestNews(),$options);
+});
+
 $bot->cmd('/p', function($coin){
         $options = ['parse_mode' => 'html','reply' => true,'disable_web_page_preview' => true];
                 return Bot::sendMessage(priceChecker($coin),$options);
